f2732ac18ec50d1329375777d729d3bb1d2a53b2
Michael <mjg.py3@gmail.com>
2015-02-25 01:57:47 -0500

    Minor optimizations and cleaning

--- problem0061.rb
+++ problem0061.rb
@@ -1,69 +1,39 @@
 #!/usr/bin/env ruby
 
-p_3 = {
-  op: ->(n){ n*(n+1)/2 },
-  lower_bound: 45,
-  upper_bound: 140
-}
-
-p_4 = {
-  op: ->(n){ n*n },
-  lower_bound: 32,
-  upper_bound: 99
-}
-
-p_5 = {
-  op: ->(n){ n*(3*n-1)/2 },
-  lower_bound: 26,
-  upper_bound: 81
-}
-
-p_6 = {
-  op: ->(n){ n*(2*n-1) },
-  lower_bound: 23,
-  upper_bound: 70
-}
-
-p_7 = {
-  op: ->(n){ n*(5*n-3)/2 },
-  lower_bound: 21,
-  upper_bound: 63
-}
-
-p_8 = {
-  op: ->(n){ n*(3*n-2)},
-  lower_bound: 17,
-  upper_bound: 58
-}
-
-def get_radi_for(func)
-  result = {
-    prefix_to_values: {},
-    suffix_to_values: {},
+$prefix_to_value = [
+  {
+    op: ->(n){ n*(n+1)/2 },
+    lower_bound: 45,
+    upper_bound: 140
+  }, {
+    op: ->(n){ n*n },
+    lower_bound: 32,
+    upper_bound: 99
+  }, {
+    op: ->(n){ n*(3*n-1)/2 },
+    lower_bound: 26,
+    upper_bound: 81
+  }, {
+    op: ->(n){ n*(2*n-1) },
+    lower_bound: 23,
+    upper_bound: 70
+  }, {
+    op: ->(n){ n*(5*n-3)/2 },
+    lower_bound: 21,
+    upper_bound: 63
+  }, {
+    op: ->(n){ n*(3*n-2)},
+    lower_bound: 17,
+    upper_bound: 58
   }
+].map do |func|
+  result = {}
   (func[:lower_bound]..func[:upper_bound]).each do |n|
     value = func[:op].(n)
     prefix = value/100
-    suffix = value%100
-    result[:prefix_to_values][prefix] = result[:prefix_to_values].fetch(prefix, []) + [value]
-    result[:suffix_to_values][suffix] = result[:suffix_to_values].fetch(suffix, []) + [value] if suffix >= 10
+    result[prefix] = result.fetch(prefix, []) + [value]
   end
   result
-end
-
-$funcs = [
-  p_3,
-  p_4,
-  p_5,
-  p_6,
-  p_7,
-  p_8,
-]
-
-$radi = []
-
-$funcs.each_with_index do |func, i|
-  $radi[i] = get_radi_for(func)
 end
 
 def permutation_map_eliminating(old_map, i)
@@ -72,27 +42,24 @@ def permutation_map_eliminating(old_map, i)
   result
 end
 
-def aux(permutation, current, chain)
-  p chain.inject(:+) and exit if !permutation.any? && chain[0]/100 == chain[-1]%100
+def aux(permutation, current, sum, first, last)
+  p sum and exit if !permutation.any? && first/100 == last%100
   suffix = current%100
+  return if suffix < 10
 
-  permutation.select { |v| v }.each do |i|
-    values = Array($radi[i][:prefix_to_values][suffix])
+  permutation.each do |i|
+    next unless i
+    ($prefix_to_value[i][suffix] || []).each do |value|
+      aux(permutation_map_eliminating(permutation, i), value, sum + value, first, value)
+    end
+  end
+end
+
+permutations_map = [0, 1, 2, 3, 4, 5]
+$prefix_to_value.each_with_index do |prefixes_to_values, i|
+  prefixes_to_values.each do |_, values|
     values.each do |value|
-      aux(permutation_map_eliminating(permutation, i), value, chain + [value])
+      aux(permutation_map_eliminating(permutations_map, i), value, value, value, nil)
     end
   end
 end
-
-def route_to_end
-  permutations_map = (0...6).to_a
-  $radi.each_with_index do |radi, i|
-    radi[:prefix_to_values].each do |perefix, values|
-      values.each do |value|
-        aux(permutation_map_eliminating(permutations_map, i), value, [value])
-      end
-    end
-  end
-end
-
-route_to_end
