f2dd74a82372402f7b104fe632357fb2cf7ce376
Michael Gilliland <mjg.py3@gmail.com>
2015-08-11 14:19:28 -0400

    More tweaking...

--- problem0064.hs
+++ problem0064.hs
@@ -1,5 +1,7 @@
 module Problem64 where
 
+import Data.List (isPrefixOf)
+
 terms :: Integer -> [Integer]
 terms n = go 1 $ floor sqrt_n
   where
@@ -10,17 +12,16 @@ terms n = go 1 $ floor sqrt_n
     den' = negate $ den - num'*a'
     a' = floor $ (sqrt_n + fromIntegral den) / fromIntegral num'
 
-sloppilyDetectPeriod n xs =
+naiveDetectPeriod n xs =
   let
-    (chunk:chunks) = [take n $ drop (n*i) xs | i <- [0..3]]
-    allChunksAreEqual = all (== chunk) chunks
+    repeated = concat $ replicate 4 $ take n xs
   in
-    if allChunksAreEqual then n else sloppilyDetectPeriod (n+1) xs
+    if repeated `isPrefixOf` xs then n else naiveDetectPeriod (n+1) xs
 
 period ts = if firstTenAreEqual then 1 else detectPeriod
   where
   firstTenAreEqual = all (== (head ts)) (take 10 ts)
-  detectPeriod = sloppilyDetectPeriod 2 ts
+  detectPeriod = naiveDetectPeriod 2 ts
 
 perfects = takeWhile (< 10000) [n*n | n <- [1..]]
 
