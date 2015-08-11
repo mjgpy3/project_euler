d8d4a31495e6ac8f7edb7eac0451fd1d5984c560
Michael Gilliland <mjg.py3@gmail.com>
2015-08-11 14:09:53 -0400

    Slight bit more cleaning

--- problem0064.hs
+++ problem0064.hs
@@ -4,21 +4,23 @@ terms :: Integer -> [Integer]
 terms n = go 1 $ floor sqrt_n
   where
   sqrt_n = sqrt $ fromIntegral n
-  go num den = a_next:go num_next den_next
+  go num den = a':go num' den'
     where
-    num_next = (n - (den * den)) `div` num
-    den_next = negate $ den - num_next*a_next
-    a_next = floor $ (sqrt_n + fromIntegral den) / fromIntegral num_next
+    num' = (n - den^2) `div` num
+    den' = negate $ den - num'*a'
+    a' = floor $ (sqrt_n + fromIntegral den) / fromIntegral num'
 
 sloppilyDetectPeriod n xs =
   let
-    (a:chunks) = [take n $ drop (n*i) xs | i <- [0..3]]
+    (chunk:chunks) = [take n $ drop (n*i) xs | i <- [0..3]]
+    allChunksAreEqual = all (== chunk) chunks
   in
-    if all (== a) chunks then n else sloppilyDetectPeriod (n+1) xs
+    if allChunksAreEqual then n else sloppilyDetectPeriod (n+1) xs
 
-period ts
-  | all (== (head ts)) (take 10 ts) = 1
-  | otherwise = sloppilyDetectPeriod 2 ts
+period ts = if firstTenAreEqual then 1 else detectPeriod
+  where
+  firstTenAreEqual = all (== (head ts)) (take 10 ts)
+  detectPeriod = sloppilyDetectPeriod 2 ts
 
 perfects = takeWhile (< 10000) [n*n | n <- [1..]]
 
